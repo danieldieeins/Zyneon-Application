8204e51ed520f109973968531cb47a8e92a671b8
danieldieeins <daniel.niesmann@outlook.com>
2023-08-08 21:30:55 +0200

    Added a bridge between the java backend and html/javascript frontend

--- src/main/java/live/nerotv/Main.java
+++ src/main/java/live/nerotv/Main.java
@@ -5,13 +5,19 @@ import live.nerotv.zyneon.launcher.frontend.WebViewApp;
 public class Main {
 
     private static WebViewApp mainWindow;
+    private static String[] arguments;
 
     public static void main(String[] args) {
+        arguments = args;
         mainWindow = new WebViewApp();
         mainWindow.start(args);
     }
 
     public static WebViewApp getMainWindow() {
         return mainWindow;
+    }
+
+    public static String[] getArguments() {
+        return arguments;
     }
 }
