33535899b99ac75800934c3e45defcd2cd4a57d9
danieldieeins <daniel.niesmann@outlook.com>
2023-08-08 17:40:41 +0200

    update

---
+++ src/main/java/live/nerotv/zyneon/launcher/backend/Modpack.java
@@ -0,0 +1,55 @@
+package live.nerotv.zyneon.launcher.backend;
+
+import java.net.URL;
+
+public class Modpack {
+
+    private URL url;
+    private String name;
+    private String version;
+    private String minecraftVersion;
+    private String id;
+
+    public Modpack(URL fileDownload) {
+        url = fileDownload;
+        //falls die URL zur JSON Datei erfolgreich runterladen konnte, die Daten der Datei entnehmen, wenn nicht Folgendes:
+        name = "Keine Verbindung";
+        version = "Unbekannt";
+        minecraftVersion = "Unbekannt";
+        id = "Unbekannt";
+    }
+
+    public void update(URL fileDownload) {
+        url = fileDownload;
+        //falls die URL zur JSON Datei erfolgreich runterladen konnte, die Daten der Datei entnehmen, wenn nicht Folgendes:
+        name = "Keine Verbindung";
+        version = "Unbekannt";
+        minecraftVersion = "Unbekannt";
+        id = "Unbekannt";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMinecraftVersion() {
+        return minecraftVersion;
+    }
+
+    public void unload() {
+        url = null;
+        name = null;
+        version = null;
+        minecraftVersion = null;
+        id = null;
+        System.gc();
+    }
+}
