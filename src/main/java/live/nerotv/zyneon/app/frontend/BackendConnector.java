082c8f67aaba3084b2d1f7d4d48b5fef6e9892f1
danieldieeins <daniel.niesmann@outlook.com>
2023-08-09 10:53:05 +0200

    added account button functionality

--- src/main/java/live/nerotv/zyneon/app/frontend/BackendConnector.java
+++ src/main/java/live/nerotv/zyneon/app/frontend/BackendConnector.java
@@ -1,13 +1,14 @@
 package live.nerotv.zyneon.app.frontend;
 
 import live.nerotv.zyneon.app.backend.installer.ForgeInstaller;
+import live.nerotv.zyneon.app.microsoft.Authentication;
 
 public class BackendConnector {
 
     public void callJavaMethod(String message) {
         System.out.println(message);
         if(message.equals("button.account")) {
-            //Für plocc: klasse.openAccountWindow(); (oder so)
+            Authentication.openAuth();
         } else if(message.contains("button.start.")) {
             String modpackid = message.replace("button.start.","");
             //Für plocc: andereKlasse.startModpack(modpackid);
--- src/main/java/live/nerotv/zyneon/app/microsoft/Authentication.java
+++ src/main/java/live/nerotv/zyneon/app/microsoft/Authentication.java
@@ -14,8 +14,7 @@ import java.util.stream.Collectors;
 
 public class Authentication {
 
-
-    public static void main(String[] args) {
+    public static void openAuth() {
         System.out.println(new Authentication().startAuth());
     }
 
