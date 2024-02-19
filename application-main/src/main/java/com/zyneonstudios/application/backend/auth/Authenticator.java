42abd26b5a019a9c6117059bea0d4a20def48f1a
nerotvlive <daniel.niesmann@outlook.com>
2024-02-20 00:19:29 +0100

    -

--- application-main/src/main/java/com/zyneonstudios/application/backend/auth/Authenticator.java
+++ application-main/src/main/java/com/zyneonstudios/application/backend/auth/Authenticator.java
@@ -55,24 +55,25 @@ public class Authenticator {
     }
 
     public boolean isLoggedIn() {
-        if(isLoggedIn) {
+        if (isLoggedIn) {
             return true;
         } else {
             authInfos = null;
-            if(saveFile!=null) {
-                if(key!=null) {
+            if (saveFile != null) {
+                if (key != null) {
                     if (saver.get("opapi.ms.a") != null || saver.get("opapi.ms.r") != null || saver.get("opapi.ms.n") != null || saver.get("opapi.ms.u") != null) {
-                        String r = (String)saver.get("opapi.ms.r");
+                        String r = (String) saver.get("opapi.ms.r");
                         try {
                             byte[] b = r.getBytes();
-                            b = AESUtil.decrypt(key,b);
-                            if(refresh(new String(b))) {
+                            b = AESUtil.decrypt(key, b);
+                            if (refresh(new String(b))) {
                                 return true;
                             } else {
                                 saver.delete("opapi.ms");
                                 return false;
                             }
-                        } catch (Exception ignore) {}
+                        } catch (Exception ignore) {
+                        }
                     }
                 }
             }
@@ -84,13 +85,13 @@ public class Authenticator {
         resolver.preAuth();
         try {
             HashMap<ZyneonAuth.AuthInfo, String> authData = ZyneonAuth.getAuthInfos();
-            authInfos = new AuthInfos(authData.get(ZyneonAuth.AuthInfo.USERNAME),authData.get(ZyneonAuth.AuthInfo.ACCESS_TOKEN),authData.get(ZyneonAuth.AuthInfo.UUID).replace("-",""));
+            authInfos = new AuthInfos(authData.get(ZyneonAuth.AuthInfo.USERNAME), authData.get(ZyneonAuth.AuthInfo.ACCESS_TOKEN), authData.get(ZyneonAuth.AuthInfo.UUID).replace("-", ""));
             save(authData);
             isLoggedIn = true;
         } catch (Exception e) {
             isLoggedIn = false;
         }
-        CompletableFuture.runAsync(()-> resolver.postAuth(authInfos.getUsername()));
+        CompletableFuture.runAsync(() -> resolver.postAuth(authInfos.getUsername()));
         return isLoggedIn;
     }
 
@@ -98,30 +99,30 @@ public class Authenticator {
         resolver.preAuth();
         try {
             HashMap<ZyneonAuth.AuthInfo, String> authData = ZyneonAuth.getAuthInfos(token);
-            authInfos = new AuthInfos(authData.get(ZyneonAuth.AuthInfo.USERNAME),authData.get(ZyneonAuth.AuthInfo.ACCESS_TOKEN),authData.get(ZyneonAuth.AuthInfo.UUID).replace("-",""));
+            authInfos = new AuthInfos(authData.get(ZyneonAuth.AuthInfo.USERNAME), authData.get(ZyneonAuth.AuthInfo.ACCESS_TOKEN), authData.get(ZyneonAuth.AuthInfo.UUID).replace("-", ""));
             save(authData);
             isLoggedIn = true;
         } catch (Exception e) {
             isLoggedIn = false;
         }
-        CompletableFuture.runAsync(()-> resolver.postAuth(authInfos.getUsername()));
+        CompletableFuture.runAsync(() -> resolver.postAuth(authInfos.getUsername()));
         return isLoggedIn;
     }
 
-    private void save(HashMap<ZyneonAuth.AuthInfo,String> authData) {
-        if(saveFile!=null) {
-            if(key!=null) {
+    private void save(HashMap<ZyneonAuth.AuthInfo, String> authData) {
+        if (saveFile != null) {
+            if (key != null) {
                 try {
                     byte[] a = AESUtil.encrypt(key, authData.get(ZyneonAuth.AuthInfo.ACCESS_TOKEN).getBytes());
                     byte[] r = AESUtil.encrypt(key, authData.get(ZyneonAuth.AuthInfo.REFRESH_TOKEN).getBytes());
                     byte[] n = AESUtil.encrypt(key, authData.get(ZyneonAuth.AuthInfo.USERNAME).getBytes());
                     byte[] u = AESUtil.encrypt(key, authData.get(ZyneonAuth.AuthInfo.UUID).getBytes());
-                    saver.set("opapi.ms.a",new String(a));
-                    saver.set("opapi.ms.r",new String(r));
-                    saver.set("opapi.ms.n",new String(n));
-                    saver.set("opapi.ms.u",new String(u));
+                    saver.set("opapi.ms.a", new String(a));
+                    saver.set("opapi.ms.r", new String(r));
+                    saver.set("opapi.ms.n", new String(n));
+                    saver.set("opapi.ms.u", new String(u));
                 } catch (Exception e) {
-                    Main.getLogger().error("[AUTH] Couldn't save login credentials: "+e.getMessage());
+                    Main.getLogger().error("[AUTH] Couldn't save login credentials: " + e.getMessage());
                 }
             }
         }
