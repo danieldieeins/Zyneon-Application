1cb2051bed6f12643c6cf2eb9344f56b4e056dde
nerotvlive <daniel.niesmann@outlook.com>
2024-02-28 23:12:30 +0100

    Scheduled next release

--- application-main/src/main/java/com/zyneonstudios/Main.java
+++ application-main/src/main/java/com/zyneonstudios/Main.java
@@ -29,7 +29,7 @@ public class Main {
     public static Architecture architecture;
 
     public static void main(String[] args) {
-        version = "2024.2.8";
+        version = "2024.3";
         String name = "?";
         architecture = getArchitecture();
         splash = new ZyneonSplash();
