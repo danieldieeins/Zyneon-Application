da264c1baeb73fc52d5b06150780e1bd889c5eb3
nerotvlive <daniel.niesmann@outlook.com>
2024-04-25 14:40:46 +0200

    Add an option to start and stop the dynamic update feature

--- content/assets/zyneon/js/functions.js
+++ content/assets/zyneon/js/functions.js
@@ -9,15 +9,20 @@ document.addEventListener('dragstart', function(e){
     e.preventDefault();
 });
 
-function sendNotification(title,text,actions) {
+function sendNotification(title,text,actions,id) {
     const template = document.getElementById("message-template");
     let card = template.cloneNode(true);
     let now = new Date();
     const date = now.getHours().toString().padStart(2, '0')+":"+now.getMinutes().toString().padStart(2, '0')+" - "+now.getFullYear()+"/"+(now.getMonth() + 1).toString().padStart(2, '0')+"/"+now.getDate().toString().padStart(2, '0');
-    card.id = date.toLowerCase().replaceAll(":","").replaceAll(" - ","").replaceAll("/","");
+    if(id!==undefined) {
+        card.id = id;
+    } else {
+        card.id = date.toLowerCase().replaceAll(":","").replaceAll(" - ","").replaceAll("/","");
+    }
     card.querySelector("#notification-title").innerText=title;
     card.querySelector("#notification-text").innerHTML=text;
     card.querySelector(".button").onclick=function () {
+        callJavaMethod("button.notification.remove."+id);
         document.getElementById(card.id).remove();
     };
     card.querySelector("#notification-actions").innerHTML=actions;
