b8b83e74f1fd4c1971754cac421c642c4c14e58b
danieldieeins <daniel.niesmann@outlook.com>
2024-10-16 20:18:10 +0200

    Adding really important event todos

--- application-main/src/main/java/com/zyneonstudios/application/events/Event.java
+++ application-main/src/main/java/com/zyneonstudios/application/events/Event.java
@@ -4,6 +4,10 @@ import java.util.UUID;
 
 public interface Event {
 
+    //TODO need more events pls dont forget
+    //TODO really pls stop making everything via request strings
+    //TODO ~a friend of this nero guy using his computer
+
     UUID getUUID();
 
     boolean execute();
